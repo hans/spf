c546b50b8321d782b6c30bc29312553ed92b71ab
Jon Gauthier <jon@gauthiers.net>
2018-01-14 14:09:54 -0500

    build_simple_questions: prep for building a curriculum

--- clevr_situated/build_simple_questions.py
+++ clevr_situated/build_simple_questions.py
@@ -9,31 +9,50 @@ import json
 import random
 
 
-def make_questions(scene):
+def yield_base_references(scene):
+  for i, obj in enumerate(scene["objects"]):
+    yield "the %s" % obj["shape"], i
+
+
+def yield_1adj_references(scene):
+  for i, obj in enumerate(scene["objects"]):
+    for prop in ["material", "color", "size"]:
+      for base_noun in [obj["shape"], "object"]:
+        yield "the %s %s" % (obj[prop], base_noun), i
+
+
+REFERENCE_FNS = [yield_base_references, yield_1adj_references]
+REFERENCE_FNS = {fn.__name__: fn for fn in REFERENCE_FNS}
+
+
+def make_questions(scene, reference_fns):
   """
   Given a CLEVR scene, generate simple questions.
   """
 
   # Find unique referents.
-  referents = defaultdict(list)
-  for i, obj in enumerate(scene["objects"]):
-    for prop in ["material", "color", "size"]:
-      for base_noun in [obj["shape"], "object"]:
-        referents[obj[prop], base_noun].append(i)
+  referents = defaultdict(set)
+  for reference_fn in reference_fns:
+    for utt, obj_idx in reference_fn(scene):
+      referents[utt].add(obj_idx)
 
   # Only yield questions which have a unique referent.
-  questions = [(scene["image_index"], "the %s %s" % q, referents[0])
-               for q, referents in referents.items()
+  questions = [(scene["image_index"], utt, next(iter(referents)))
+               for utt, referents in referents.items()
                if len(referents) == 1]
   return questions
 
 
 def main(args):
+  reference_fns = args.reference or ["yield_1adj_references"]
+  reference_fns = [REFERENCE_FNS[fn] for fn in reference_fns]
+
   with open(args.scenes_path, "r") as scenes_f:
     scene_data = json.load(scenes_f)
   scenes = scene_data["scenes"]
 
-  questions = list(itertools.chain.from_iterable(map(make_questions, scenes)))
+  questions = list(itertools.chain.from_iterable(
+    (make_questions(scene, reference_fns)) for scene in scenes))
   random.shuffle(questions)
 
   enumerator = range(args.limit) if args.limit > 0 else itertools.count()
@@ -59,5 +78,6 @@ if __name__ == '__main__':
   p.add_argument("scenes_path")
   p.add_argument("out_path")
   p.add_argument("--limit", default=0, type=int)
+  p.add_argument("--reference", choices=REFERENCE_FNS.keys(), action="append")
 
   main(p.parse_args())
