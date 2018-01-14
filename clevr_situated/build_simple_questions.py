bfb2f6753102a2410730b9d942628d39fb0a9f0e
Jon Gauthier <jon@gauthiers.net>
2018-01-14 15:52:36 -0500

    build_simple_questions: add a third phase for generating basic property-query questions

--- clevr_situated/build_simple_questions.py
+++ clevr_situated/build_simple_questions.py
@@ -9,50 +9,63 @@ import json
 import random
 
 
-def yield_base_references(scene):
+PROPERTY_TYPES = frozenset(["material", "color", "size", "shape"])
+
+
+def make_obj_answer(idx):
+  return {"type": "object", "index": idx}
+
+
+def yield_base_questions(scene):
   for i, obj in enumerate(scene["objects"]):
-    yield "the %s" % obj["shape"], i
+    yield "the %s" % obj["shape"], make_obj_answer(i)
 
-
-def yield_1adj_references(scene):
+def yield_1adj_questions(scene):
   for i, obj in enumerate(scene["objects"]):
     for prop in ["material", "color", "size"]:
       for base_noun in [obj["shape"], "object"]:
-        yield "the %s %s" % (obj[prop], base_noun), i
+        yield "the %s %s" % (obj[prop], base_noun), make_obj_answer(i)
+
+def yield_attr_questions(scene):
+  for i, obj in enumerate(scene["objects"]):
+    for query_prop in ["color", "material", "size"]:
+      for mod_prop in PROPERTY_TYPES - set([query_prop, "shape"]):
+        for base_noun in [obj["shape"], "object"]:
+          yield "what %s is the %s %s" % (query_prop, obj[mod_prop], base_noun), \
+              obj[query_prop]
+
+QUESTION_FNS = [yield_base_questions, yield_1adj_questions, yield_attr_questions]
+QUESTION_FNS = {fn.__name__: fn for fn in QUESTION_FNS}
 
 
-REFERENCE_FNS = [yield_base_references, yield_1adj_references]
-REFERENCE_FNS = {fn.__name__: fn for fn in REFERENCE_FNS}
-
-
-def make_questions(scene, reference_fns):
+def make_questions(scene, question_fns):
   """
   Given a CLEVR scene, generate simple questions.
   """
 
-  # Find unique referents.
-  referents = defaultdict(set)
-  for reference_fn in reference_fns:
-    for utt, obj_idx in reference_fn(scene):
-      referents[utt].add(obj_idx)
+  # Find questions with unique answers.
+  answers = defaultdict(set)
+  for question_fn in question_fns:
+    for utt, ans in question_fn(scene):
+      answers[utt].add(ans)
 
   # Only yield questions which have a unique referent.
-  questions = [(scene["image_index"], utt, next(iter(referents)))
-               for utt, referents in referents.items()
-               if len(referents) == 1]
+  questions = [(scene["image_index"], utt, next(iter(answers_i)))
+               for utt, answers_i in answers.items()
+               if len(answers_i) == 1]
   return questions
 
 
 def main(args):
-  reference_fns = args.reference or ["yield_1adj_references"]
-  reference_fns = [REFERENCE_FNS[fn] for fn in reference_fns]
+  question_fns = args.question or ["yield_1adj_questions"]
+  question_fns = [QUESTION_FNS[fn] for fn in question_fns]
 
   with open(args.scenes_path, "r") as scenes_f:
     scene_data = json.load(scenes_f)
   scenes = scene_data["scenes"]
 
   questions = list(itertools.chain.from_iterable(
-    (make_questions(scene, reference_fns)) for scene in scenes))
+    (make_questions(scene, question_fns)) for scene in scenes))
   random.shuffle(questions)
 
   enumerator = range(args.limit) if args.limit > 0 else itertools.count()
@@ -63,9 +76,9 @@ def main(args):
         {
           "question_index": i,
           "image_index": image_index,
-          "answer": {"type": "object", "index": obj_index},
-          "question": question
-        } for i, (image_index, question, obj_index) in zip(enumerator, questions)
+          "answer": answer,
+          "question": question,
+        } for i, (image_index, question, answer) in zip(enumerator, questions)
         ]
       }
 
@@ -78,6 +91,6 @@ if __name__ == '__main__':
   p.add_argument("scenes_path")
   p.add_argument("out_path")
   p.add_argument("--limit", default=0, type=int)
-  p.add_argument("--reference", choices=REFERENCE_FNS.keys(), action="append")
+  p.add_argument("--question", choices=QUESTION_FNS.keys(), action="append")
 
   main(p.parse_args())
