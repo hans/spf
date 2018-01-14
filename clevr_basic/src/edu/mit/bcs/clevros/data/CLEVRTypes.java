4415d482a5fb20ae64c5fec453acf9917f22eeee
Jon Gauthier <jon@gauthiers.net>
2018-01-14 15:35:54 -0500

    CLEVRTypes: wow, fix color list to actually match CLEVR....

--- clevr_basic/src/edu/mit/bcs/clevros/data/CLEVRTypes.java
+++ clevr_basic/src/edu/mit/bcs/clevros/data/CLEVRTypes.java
@@ -7,7 +7,7 @@ public class CLEVRTypes {
 
     public static final String[] SHAPES = new String[] { "sphere", "cylinder", "cube" };
     public static final String[] SIZES = new String[] { "small", "large" };
-    public static final String[] COLORS = new String[] { "red", "blue", "green", "pink", "purple", "brown", "black" };
+    public static final String[] COLORS = new String[] { "green", "purple", "blue", "brown", "gray", "cyan", "yellow", "red" };
     public static final String[] MATERIALS = new String[] { "metal", "rubber" };
 
     public static final Map<String, String[]> PROPERTIES = new HashMap<>();
