663ed3f1bbc916f11ad0ab2ae9e9f9c15c844646
Jon Gauthier <jon@gauthiers.net>
2018-01-30 19:53:21 -0500

    let's not get too ambitious now

--- clevr_basic/src/edu/mit/bcs/clevros/data/CLEVRTypes.java
+++ clevr_basic/src/edu/mit/bcs/clevros/data/CLEVRTypes.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public class CLEVRTypes {
 
-    public static final String[] SHAPES = new String[100];
-    public static final String[] COLORS = new String[100];
+    public static final String[] SHAPES = new String[20];
+    public static final String[] COLORS = new String[20];
     static {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 20; i++) {
             SHAPES[i] = "s" + i;
             COLORS[i] = "c" + i;
         }
