c522891fc7e3aa50409f480ba87616fce20dca41
Jon Gauthier <jon@gauthiers.net>
2018-02-01 12:07:52 -0500

    make separate overhypothesis graph

--- clevr_situated/summarize_all.py
+++ clevr_situated/summarize_all.py
@@ -33,20 +33,35 @@ def main(args):
   ax.set_xlim(XLIM)
   ax.set_ylim(YLIM)
 
-  # Plot p(shape|N/N) belief on right axis.
+  fig.tight_layout()
+  fig.savefig("%s.learning.png" % args.img_out)
+
+  ###############
+
+  fig, ax = plt.subplots()
+  results_model = results[results.condition == "model"]
+  results_baseline = results[results.condition == "ccg"]
+  results_diff = results_model.groupby("i").value.agg("mean") - results_baseline.groupby("i").value.agg("mean")
+
+  results_diff.plot(y="value", ax=ax, legend=False)
+
+  ax.set_xlabel("# examples")
+  ax.set_ylabel("Difference in recall (model - CCG)")
+
   right_ax = ax.twinx()
-  right_ax.set_ylabel("p(shape|N/N)")
-  # Hack: align grid
-  right_ax.set_yticks(np.linspace(ax.get_yticks()[0], ax.get_yticks()[-1], len(ax.get_yticks())))
+  right_ax.set_ylabel("p(color|N/N)")
   posterior = results[results.condition != "ccg"]
   posterior = posterior[posterior.variable == "p(shape|N/N)"]
   posterior = posterior.groupby("i").value.agg(["mean", standard_error])
   posterior.plot(y="mean", ax=right_ax, color="#aaaaaa", zorder=1, legend=False)
   right_ax.fill_between(posterior.index, posterior["mean"] - posterior.standard_error, posterior["mean"] + posterior.standard_error, color="#ccccccaa")
-  right_ax.set_ylim(YLIM)
+  right_ax.set_ylim(0, 1)
+  # Align axes.
+  right_ax.set_yticks(np.linspace(right_ax.get_yticks()[0], right_ax.get_yticks()[-1], len(ax.get_yticks())))
+  right_ax.grid(None)
 
-  plt.tight_layout()
-  plt.savefig(args.img_out)
+  fig.tight_layout()
+  fig.savefig("%s.overhyp.png" % args.img_out)
 
 
 if __name__ == '__main__':
